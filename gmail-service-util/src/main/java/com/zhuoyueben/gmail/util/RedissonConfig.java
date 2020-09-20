9ae3d9cfc90a2bfd0012429ce903c82fd222ba18
zhouyueben <2487186110@qq.com>
2020-09-20 22:42:10 +0800

    服务模块提交

---
+++ gmail-service-util/src/main/java/com/zhuoyueben/gmail/util/RedissonConfig.java
@@ -0,0 +1,30 @@
+package com.zhuoyueben.gmail.util;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description
+ * @Author TeaBen
+ * @Date 2020-05-11 16:46
+ */
+@Configuration
+public class RedissonConfig {
+    @Value("${spring.redis.host:0}")
+    private String host;
+
+    @Value("${spring.redis.port:6379}")
+    private String port;
+
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://"+host+":"+port);
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+}
